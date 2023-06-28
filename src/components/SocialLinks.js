01e944cdff98fb37d633c691d30751d41d21d082
aditya24 <adityadawar1999@gmail.com>
2023-06-28 20:49:05 +0530

    res

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/resume-1.pdf",
+      href: "/resume2.pdf",
       style: "rounded-br-md",
       download: true,
     },
